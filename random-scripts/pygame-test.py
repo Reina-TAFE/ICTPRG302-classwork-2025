5b0ba2e14e54c7453faba5ac722da8b683c00928
Reina <20066312@tafe.wa.edu.au>
2025-03-21 16:28:00 +0800

    WIP

--- random-scripts/pygame-test.py
+++ random-scripts/pygame-test.py
@@ -1,2 +1,19 @@
+# import substitution
 import pygame
-import substitution
+from pygame.locals import *
+
+pygame.init()
+run = True
+while run:
+
+
+    # screen = pygame.display.set_mode([600, 600])
+    clock = pygame.time.Clock()
+
+    window = pygame.display.set_mode([600, 600])
+    bg_colour = (64, 64, 128)
+
+    window.fill((255, 255, 255))
+
+    pygame.draw.rect(window, (64, 64, 128), (100, 100, 400, 100), 0)
+pygame.display.update()
