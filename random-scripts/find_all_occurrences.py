c292ba998944f6a8f2412c97fc4257e5f597e76f
Veyren <20066312#tafe.wa.edu.au>
2025-05-09 02:32:23 +0800

    find_all_occurrences.py added

---
+++ random-scripts/find_all_occurrences.py
@@ -0,0 +1,43 @@
+
+def find_all_occurrences(target_word):
+    """Returns a dictionary of letters in target_word, with each unique letter as the keys
+    and a list of the position(s) of each occurrence as the values
+
+     Parameters
+    ----------
+    guess : string
+
+    Returns
+    -------
+    result : dictionary of letter : [occurrences] pairs
+
+    Examples
+    --------
+    >>> find_all_occurrences("world")
+    {'w': [0], 'o': [1], 'r': [2], 'l': [3], 'd': [4]}
+
+    >>> find_all_occurrences("hello")
+    {'h': [0], 'e': [1], 'l': [2, 3], 'o': [4]}
+
+    >>> find_all_occurrences("hilly")
+    {'h': [0], 'i': [1], 'l': [2, 3], 'y': [4]}
+    """
+
+    letter_dict = {}
+    letter_list = list(target_word)
+    for letter_position in range(len(letter_list)):
+        if letter_list[letter_position] not in letter_dict.keys():
+            letter_dict[letter_list[letter_position]] = [letter_position]
+        else:
+            letter_dict[letter_list[letter_position]].append(letter_position)
+    return letter_dict
+
+def main(test=True):
+    # if test:
+    #     import doctest
+    #     return doctest.testmod()
+    find_all_occurrences("hello")
+
+
+if __name__ == '__main__':
+    print(main(test=False))
