95e1040a6d71453791ddfa6705d68fd3ac29ba9d
Veyren <20066312#tafe.wa.edu.au>
2025-05-09 04:22:01 +0800

    new score_guess() added

--- random-scripts/find_all_occurrences.py
+++ random-scripts/find_all_occurrences.py
@@ -32,6 +32,49 @@ def find_all_occurrences(target_word):
             letter_dict[letter_list[letter_position]].append(letter_position)
     return letter_dict
 
+def score_guess2(guess, target):
+    """Returns a dictionary of letters in target_word, with each unique letter as the keys
+    and a list of the position(s) of each occurrence as the values
+
+     Parameters
+    ----------
+    guess : string
+
+    Returns
+    -------
+    result : dictionary of letter : [occurrences] pairs
+
+    Examples
+    --------
+    >>> score_guess2()("world", "world")
+    [2, 2, 2, 2, 2]
+
+    >>> score_guess2("world", "hello")
+    [0, 1, 0, 2, 0]
+
+    >>> score_guess2()("hilly", "world")
+    {'h': [0], 'i': [1], 'l': [2, 3], 'y': [4]}
+    """
+    score = [0] * len(guess)
+    guess_dict = find_all_occurrences(guess)
+    target_dict = find_all_occurrences(target)
+    for letter in guess_dict.keys():
+        if letter in target_dict.keys() and len(guess_dict[letter]) <= len(target_dict[letter]):
+            for position in guess_dict[letter]:
+                if position in target_dict[letter]:
+                    score[position] = 2
+                    target_dict[letter].remove(position)
+                else:
+                    score[position] = 1
+        elif letter in target_dict.keys():
+            for position in target_dict[letter]:
+                if position not in guess_dict[letter]:
+                    for score_position in guess_dict[letter][:len(guess_dict[letter])-1]:
+                        score[score_position] = 1
+                else:
+                    score[position] = 2
+    return score
+
 def main(test=True):
     # if test:
     #     import doctest
